a198781d604ed1c7b2579edc7258d45fb0e71517
0xZzzZ <JeanCrypto619@gmail.com>
2022-11-04 15:04:04 +0100

    Add Deployer

--- libs/deployer.js
+++ libs/deployer.js
@@ -1,9 +1,9 @@
-export const deployerContract = `{
+export const deployerContract = (base64wasm) =>
+`import { createSC, generateEvent, fileToBase64 } from './@massalabs/massa-as-sdk';
 
-    import { createSC, generateEvent, fileToBase64 } from '@massalabs/massa-as-sdk';
-    
-    export function main(_args: string): i32 {
-        const bytes = fileToBase64('./main.wasm');
-        const websiteDeployer = createSC(bytes);
-        return websiteDeployer;
-    }`
+export function main(_args: string): i32 {
+    const bytes = "${base64wasm}";
+    const websiteDeployer = createSC(bytes);
+    generateEvent(\`Contract deploy at : \${websiteDeployer._value}\`);
+    return 0;
+    }`;
